021c81e71a3582768e16e326a3ff0929cd4e06c0
ProgramatorPL <32798285+ProgramatorPL@users.noreply.github.com>
2025-09-09 09:44:49 +0200

    Quick Fixes

--- Install Requirements.bat
+++ Install Requirements.bat
@@ -1,3 +1,10 @@
 @echo off
+cd /d "%~dp0"
+echo Checking Python version...
+python --version
+echo Checking pip version...
+python -m pip --version
+echo.
 echo Installing requirements please wait patiently...
-python install -r requirements.txt
+python -m pip install -r requirements.txt
+pause
